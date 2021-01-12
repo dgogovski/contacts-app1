7c8579db1a4116acf7e82c50bd775838e81f6ef0
DaniRogachev <darogachev03@gmail.com>
2021-01-12 13:19:12 +0200

    New

--- database.py
+++ database.py
@@ -1,39 +1,26 @@
-from contact import Contact
+import sqlite3
 
+DB_NAME = 'contacts.db'
 
-# TODO update and delete methods
-class Database:
-    def __init__(self):
-        self.__contacts = []
-        self.__latest_contact_id = 0
-        self.add_contact(Contact('Daniel', '123', 'note1'))
-        self.add_contact(Contact('Devin', '456', 'note2'))
+conn = sqlite3.connect(DB_NAME)
 
-    def get_contacts(self):
-        return self.__contacts
+conn.cursor().execute('''
+	CREATE TABLE IF NOT EXISTS users
+	(
+		user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username TEXT UNIQUE NOT NULL,
+		number TEXT UNIQUE NOT NULL,
+		password TEXT NOT NULL
+	)		
+''')
 
-    def get_contact_by_id(self, id_):
-        for contact in self.__contacts:
-            if contact.id == id_:
-                return contact
-        print('No contact with such id')
-        return None
+conn.commit()
 
-    # TODO check if contact has no id
-    def add_contact(self, contact):
-        if contact.id != -1:
-            return False
-        contact.id = self.__latest_contact_id
-        self.__latest_contact_id += 1
-        self.__contacts.append(contact)
-        return True
+class DB:
+	def __enter__(self):
+		self.conn = sqlite3.connect(DB_NAME)
+		return self.conn.cursor()
 
-    def update_contact(self, contact_id, new_name, new_number, new_note):
-        contact = self.get_contact_by_id(contact_id)
-        # TODO check if the contact exists
-        contact.name = new_name
-        contact.number = new_number
-        contact.note = new_note
+	def __exit__(self, type, value, traceback):
+		self.conn.commit()
 
-    def delete_contact(self, contact_id):
-        self.__contacts.remove(self.get_contact_by_id(contact_id))
